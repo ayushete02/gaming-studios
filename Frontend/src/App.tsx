f813f4d64e582388009e05063dcb76833009ff43
Ayush Shete <75811912+ayushete02@users.noreply.github.com>
2023-06-18 21:31:46 +0530

    Transfering push chat from dashboard to all Windows

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,6 +6,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import { WagmiConfig, createClient } from "wagmi";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultClient,
+} from "connectkit";
+import { Chat } from "@pushprotocol/uiweb";
+import { useAccount } from "wagmi";
+
+const client = createClient(
+  getDefaultClient({
+    appName: "Push Protocol Chat",
+  })
+);
+
 
 export default function App() {
   const router = createBrowserRouter(
@@ -24,8 +39,22 @@ export default function App() {
 }
 
 function Root() {
+   const { address } = useAccount();
   return (
     <main className="relative">
+       <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <Chat
+          account={address}
+          supportAddress="0xfC001B20Db6195b148cfc4F7685091931D93cD93"
+          apiKey={'jVPMCRom1B.iDRMswdehJG7NpHDiECIHwYMMv6k2KzkPJscFIDyW8TtSnk4blYnGa8DIkfuacU0'}
+          env="staging"
+        />
+        <div className="App">
+          <ConnectKitButton />
+        </div>
+      </ConnectKitProvider>
+    </WagmiConfig>
       <div>
         <Outlet />
       </div>
